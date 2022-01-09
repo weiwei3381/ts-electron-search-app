77bd8f8f5c1a5753da5a0242483e0ebb035a9c3b
wowbat <weiwei3381@live.com>
2022-01-09 23:55:42 +0800

    add: 增加electron打包功能, 能够正常打包

--- src/preload.ts
+++ src/preload.ts
@@ -1,3 +1,5 @@
+import nodejieba from 'nodejieba'
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
@@ -7,7 +9,7 @@ window.addEventListener("DOMContentLoaded", () => {
       element.innerText = text;
     }
   };
-
+  replaceText("nodejieba", nodejieba.cut("在不影响代码正确执行的前提下，最优化的代码压缩配置为如下").join("|"))
   for (const type of ["chrome", "node", "electron"]) {
     replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
   }
